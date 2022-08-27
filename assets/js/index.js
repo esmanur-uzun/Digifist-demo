d1403d1dfdb0eb47fc95e132df0fdb286610561a
esmanurbtmz <esmabitmez@hotmail.com>
2022-08-27 10:24:40 +0300

    theme is complete

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    centeredSlides: true,
+    loop:true,
+    pagination: {
+    //   el: ".swiper-pagination",
+      clickable: true,
+    },
+});
